3b99ff8c8360610d580ba0fbd113f0c769adecdb
Rafael Faustini <contato@rafaelfaustini.com.br>
2021-01-09 15:15:32 -0300

    Merge Fix
    Merge deleted a bracket

--- AkumaNoMi/src/main/java/br/com/rafaelfaustini/akumanomi/utils/Utils.java
+++ AkumaNoMi/src/main/java/br/com/rafaelfaustini/akumanomi/utils/Utils.java
@@ -13,14 +13,14 @@ public class Utils {
         }
         return ChatColor.translateAlternateColorCodes('&', str);
     }
-    public static boolean rollD(int number){
+    public static boolean rollD(int number) {
         Random r = new Random();
         int random = r.nextInt(number - 2) + 1;
-        if(random == 5){
+        if (random == 5) {
             return true;
         }
         return false;
-
+    }
     public static void TryException(Exception e){
         System.out.println("[AkumaNoMi] There was an error loading the plugin");
         if(Debug.getEnabled()){
